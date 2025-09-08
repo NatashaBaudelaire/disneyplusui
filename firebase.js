b726bf7bfa63424ec0151798a288dc4e416a1d28
NatashaBaudelaire <natashabaudelairestudies@outlook.com>
2025-09-08 02:23:08 -0300

    Project

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtDn5MaWgdpIB9-QxdWTS5Cn_DRNw8-TY",
-  authDomain: "disneyplusstreaminginterface.firebaseapp.com",
-  projectId: "disneyplusstreaminginterface",
-  storageBucket: "disneyplusstreaminginterface.firebasestorage.app",
-  messagingSenderId: "835935667855",
-  appId: "1:835935667855:web:c56ad0fa7a5ec0cc5152fa",
-  measurementId: "G-HW7RS38VTF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-export { auth };
+export { auth, app };
